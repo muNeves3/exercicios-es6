f46e481483d0a59b60c1d14982f844c871b07a5d
Murilo de Souza Neves <murilogrilosn@gmail.com>
2020-06-20 12:43:43 -0300

    exercicio utilizando classes

---
+++ ex-1.js
@@ -0,0 +1,25 @@
+class Usuario {
+  constructor(email, senha){
+    this.email = email;
+    this.senha = senha;
+  }
+  isAdmin() {
+    if(this.admin == true){
+      return ('É administrador');
+    } else {
+      return('Não é administrador');
+    }
+  }
+}
+
+class Admin extends Usuario{
+  constructor() {
+    super();
+    this.admin = true;
+  }
+}
+
+const User1 = new Usuario('email@teste.com', 'senha123');
+const Adm1 = new Admin('email@teste.com', 'senha123');
+console.log(User1.isAdmin()) // false
+console.log(Adm1.isAdmin()) // true
